2648521761adce1788ccccf8ec823e32fd7a7288
Dakota Upchurch <cheyenneupchurch@hotmail.com>
2021-03-08 18:59:10 -0600

    set up javascript structure

--- src/scripts/data/DataManager.js
+++ src/scripts/data/DataManager.js
@@ -19,3 +19,18 @@ const loggedInUser = {
 export const getLoggedInUser = () => {
 	return loggedInUser;
 }
+
+// export const post = (postObj) => {
+// 	return fetch("http://localhost:8088/posts", {
+//         method: "POST",
+// 		headers: {
+// 			"Content-Type": "application/json"
+// 		},
+// 		body: JSON.stringify(postObj)
+//     })
+//     .then(response => response.json())
+//     .then(parsedResponse => {
+//         // do something with response here
+//         return parsedResponse;
+//     })
+// }
